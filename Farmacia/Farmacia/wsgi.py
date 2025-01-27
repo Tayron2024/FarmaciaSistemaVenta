3ce3cdeab3fc292da27450e13155af0fb5f7c9cc
Tayron Morales <169932075+Tayron2024@users.noreply.github.com>
2025-01-27 16:20:54 -0500

    Delete Farmacia directory

--- Farmacia/Farmacia/wsgi.py
+++
@@ -1,6 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Farmacia.settings')  # Asegúrate de que esto coincida con el nombre del directorio de tu proyecto.
-
-application = get_wsgi_application()
